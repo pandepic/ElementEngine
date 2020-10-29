0d76737c8a2627a5a273ca26da03fe1762b376c0
pandepic <39251913+pandepic@users.noreply.github.com>
2020-10-30 02:32:55 +1100

    AI state machine and GOAP controller

---
+++ AI/GOAPController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PandaEngine
+{
+    public class GOAPAction<T>
+    {
+        public string Name { get; set; }
+        public List<T> Requires { get; set; }
+        public List<T> Provides { get; set; }
+
+        public int Cost { get; set; }
+
+        public GOAPAction(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+            Requires = new List<T>();
+            Provides = new List<T>();
+        }
+    } // GOAPAction
+
+    public class GOAPGoal<T>
+    {
+        public string Name { get; set; }
+        public GOAPAction<T> Result { get; set; }
+        public List<GOAPAction<T>> Actions { get; set; }
+
+        public GOAPGoal(string name)
+        {
+            Name = name;
+            Actions = new List<GOAPAction<T>>();
+        }
+    } // GOAPGoal
+
+    public class GOAPController<T>
+    {
+        public List<GOAPAction<T>> AvailableActions { get; set; }
+        public GOAPGoal<T> CurrentGoal { get; set; }
+
+        public GOAPController()
+        {
+            AvailableActions = new List<GOAPAction<T>>();
+        }
+
+        public GOAPController(List<GOAPAction<T>> availableActions)
+        {
+            AvailableActions = availableActions;
+        }
+
+        public bool TrySetGoal(List<T> Request, string name)
+        {
+            GOAPAction<T> Result = null;
+
+            for (var i = 0; i < AvailableActions.Count && Result == null; i++)
+            {
+                var action = AvailableActions[i];
+                var matches = Request.All(action.Provides.Contains);
+                
+                if (matches)
+                    Result = action;
+            }
+
+            if (Result == null)
+                return false;
+
+            var goal = new GOAPGoal<T>(name)
+            {
+                Result = Result
+            };
+
+            if (TrySetGoalActions(goal))
+            {
+                CurrentGoal = goal;
+                return true;
+            }
+            else
+                return false;
+
+        } // TrySetGoal
+
+        protected bool TrySetGoalActions(GOAPGoal<T> goal)
+        {
+            goal.Actions.Clear();
+
+            var currentAction = goal.Result;
+
+            var pathFound = false;
+
+            while (!pathFound)
+            {
+                if (currentAction.Requires.Count == 0)
+                {
+                    goal.Actions.Add(currentAction);
+                    return true;
+                }
+
+                GOAPAction<T> checkAction = null;
+
+                for (var i = 0; i < AvailableActions.Count; i++)
+                {
+                    var availableAction = AvailableActions[i];
+
+                    if (availableAction.Provides.ListCompare(currentAction.Requires))
+                    {
+                        if (checkAction == null || checkAction.Cost > availableAction.Cost)
+                            checkAction = availableAction;
+                    }
+                }
+
+                if (checkAction == null)
+                    return false;
+                else
+                {
+                    goal.Actions.Add(currentAction);
+                    currentAction = checkAction;
+                }
+            }
+
+            return false;
+
+        } // SetGoalActions
+
+    } // GOAPController
+}
---
+++ AI/SimpleStateMachine.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Generic;
+
+namespace PandaEngine
+{
+    public class SimpleStateBase
+    {
+        public string Name;
+        public SimpleStateMachine Parent;
+
+        public SimpleStateBase() { }
+
+        public SimpleStateBase(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Begin() { }
+        public virtual void Update(GameTimer gameTimer) { }
+        public virtual void End() { }
+    } // SimpleStateBase
+
+    public class SimpleStateTimedBase : SimpleStateBase
+    {
+        public float Duration;
+
+        public SimpleStateTimedBase() { }
+        public SimpleStateTimedBase(string name) : base(name) { }
+
+        public override void Begin()
+        {
+            base.Begin();
+        }
+
+        public override void Update(GameTimer gameTimer)
+        {
+            Duration -= gameTimer.DeltaMS;
+
+            if (Duration <= 0)
+                EndDuration();
+        }
+
+        public override void End()
+        {
+            base.End();
+        }
+
+        public virtual void EndDuration() { }
+    } // SimpleStateTimedBase
+
+    public class SimpleStateMachine
+    {
+        public Dictionary<string, SimpleStateBase> States = new Dictionary<string, SimpleStateBase>();
+
+        public SimpleStateBase CurrentState, NextState, PrevState;
+
+        public void RegisterState(SimpleStateBase state)
+        {
+            if (States.ContainsKey(state.Name))
+                return;
+
+            state.Parent = this;
+            States.Add(state.Name, state);
+        }
+
+        public void RemoveState(SimpleStateBase state)
+        {
+            RemoveState(state.Name);
+        }
+
+        public void RemoveState(string name)
+        {
+            if (!States.ContainsKey(name))
+                return;
+
+            States.Remove(name);
+        }
+
+        public void SetState(SimpleStateBase state)
+        {
+            SetState(state.Name);
+        }
+
+        public void SetState(string name)
+        {
+            if (!States.ContainsKey(name))
+                return;
+
+            SetCurrentStateInternal(States[name]);
+        }
+
+        public void SetState<T>() where T : SimpleStateBase
+        {
+            foreach (var kvp in States)
+            {
+                if (kvp.Value is T)
+                {
+                    SetCurrentStateInternal(kvp.Value);
+                    return;
+                }
+            }
+        }
+
+        public T GetState<T>() where T : SimpleStateBase
+        {
+            foreach (var kvp in States)
+            {
+                if (kvp.Value is T)
+                    return (T)kvp.Value;
+            }
+
+            return null;
+        }
+
+        public void Start(SimpleStateBase state)
+        {
+            Start(state.Name);
+        }
+
+        public void Start(string name)
+        {
+            if (!States.ContainsKey(name))
+                return;
+
+            NextState = null;
+            PrevState = null;
+            SetCurrentStateInternal(States[name]);
+        }
+
+        public void Start<T>() where T : SimpleStateBase
+        {
+            foreach (var kvp in States)
+            {
+                if (kvp.Value is T)
+                {
+                    Start(kvp.Value.Name);
+                    return;
+                }
+            }
+        }
+
+        public void Update(GameTimer gameTimer)
+        {
+            CurrentState?.Update(gameTimer);
+
+            if (NextState != null)
+            {
+                SetCurrentStateInternal(NextState);
+                NextState = null;
+            }
+        }
+
+        protected void SetCurrentStateInternal(SimpleStateBase state)
+        {
+            CurrentState?.End();
+            PrevState = CurrentState;
+
+            CurrentState = state;
+            CurrentState.Begin();
+        }
+    } // SimpleStateMachine
+}
