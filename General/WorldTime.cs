7da43c230557df9c7fbd064f4ff95226b499aa8a
pandepic <polyoutube@gmail.com>
2020-11-29 00:51:19 +1100

    world time progress

--- General/WorldTime.cs
+++ General/WorldTime.cs
@@ -10,6 +10,8 @@ namespace ElementEngine
         public int Index { get; set; }
         public int Days { get; set; }
 
+        internal int TotalDaysBeforeMonth { get; set; }
+
         public WorldTimeMonth(string name, int days)
         {
             Name = name;
@@ -39,6 +41,7 @@ namespace ElementEngine
         public ulong TotalTicks { get; set; } = 0;
         public ulong TicksPerSecond { get; set; } = 1;
         public ulong SecondsPerMinute { get; set; } = 60;
+        public ulong TicksPerMinute => TicksPerSecond * SecondsPerMinute;
         public ulong MinutesPerHour { get; set; } = 60;
         public ulong HoursPerDay { get; set; } = 24;
 
@@ -48,15 +51,36 @@ namespace ElementEngine
         public ulong TotalDays => TotalTicks / (TicksPerSecond * SecondsPerMinute * MinutesPerHour * HoursPerDay);
         public ulong TotalYears => TotalDays / (ulong)DaysPerYear;
 
+        public int DayOfYear => (int)(TotalDays - (TotalYears * (ulong)DaysPerYear));
+        
+        public WorldTimeMonth CurrentMonth
+        {
+            get
+            {
+                var daysCounter = DayOfYear;
+                for (var i = 0; i < Months.Count; i++)
+                {
+                    var month = Months[i];
+                    daysCounter -= month.Days;
+                    if (daysCounter <= 0)
+                        return month;
+                }
+                return null;
+            }
+        }
+
+        public int DayOfMonth => (int)TotalDays - CurrentMonth.TotalDaysBeforeMonth;
+
         public int DaysPerYear { get; private set; } = 0;
         public int DaysPerWeek => Days.Count;
         public int CurrentDayIndex => (int)TotalDays % DaysPerWeek;
         public string CurrentDayName => Days[CurrentDayIndex];
 
-        public WorldTime() : this(DefaultDays, DefaultMonths) { }
+        public WorldTime(ulong ticks = 0) : this(DefaultDays, DefaultMonths, ticks) { }
 
-        public WorldTime(List<string> days, List<WorldTimeMonth> months)
+        public WorldTime(List<string> days, List<WorldTimeMonth> months, ulong ticks = 0)
         {
+            TotalTicks = ticks;
             Days = days;
             Months = months;
 
@@ -68,5 +92,28 @@ namespace ElementEngine
         {
             TotalTicks += amount;
         }
+
+        // %t - minutes
+        // %h - hours
+        // %d - current day
+        // %D - current day name
+        // %m - current month index
+        // %M - current month name
+        // %y - year
+        //public string GetTimeAsString(string format)
+        //{
+        //    string ret = format;
+
+        //    ret = ret.Replace("%t", CurrentMinutes.ToString());
+        //    ret = ret.Replace("%h", CurrentHours.ToString());
+        //    ret = ret.Replace("%d", CurrentDay.ToString());
+        //    ret = ret.Replace("%D", Days[CurrentDayIndex]);
+        //    ret = ret.Replace("%m", CurrentMonthIndex.ToString());
+        //    ret = ret.Replace("%M", Months.ElementAt(CurrentMonthIndex).Key);
+        //    ret = ret.Replace("%y", CurrentYear.ToString());
+
+        //    return ret;
+        //}
+
     } // WorldTime
 }
