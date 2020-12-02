8c7b168cff42c09f3fff2af7e73bf5e3317ff2b2
pandepic <39251913+pandepic@users.noreply.github.com>
2020-12-02 20:30:35 +1100

    imgui support

---
+++ UI/IMGUI/IMGUIManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Veldrid;
+
+namespace ElementEngine.UI
+{
+    public static class IMGUIManager
+    {
+        public static ImGuiRenderer Renderer { get; set; }
+
+        public static void Setup()
+        {
+            Renderer = new ImGuiRenderer(ElementGlobals.GraphicsDevice,
+                ElementGlobals.GraphicsDevice.SwapchainFramebuffer.OutputDescription,
+                ElementGlobals.TargetResolutionWidth, ElementGlobals.TargetResolutionHeight);
+
+            Renderer.RecreateFontDeviceTexture();
+        }
+
+        public static void Update(GameTimer gameTimer)
+        {
+            if (InputManager.PrevSnapshot == null)
+                return;
+
+            Renderer.Update(gameTimer.DeltaS, InputManager.PrevSnapshot);
+        }
+
+        public static void Draw()
+        {
+            Renderer.Render(ElementGlobals.GraphicsDevice, ElementGlobals.CommandList);
+        }
+    }
+}
