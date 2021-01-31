d39533caec9eb36ba6f5d9bd0f0db6f4f927f4e4
pandepic <39251913+pandepic@users.noreply.github.com>
2021-01-31 19:54:16 +1100

    vector2i int math

--- Math/Vector2I.cs
+++ Math/Vector2I.cs
@@ -81,6 +81,12 @@ namespace ElementEngine
         public static Vector2I operator /(Vector2I p1, Vector2I p2) => new Vector2I(p1.X / p2.X, p1.Y / p2.Y);
         public static Vector2I operator %(Vector2I p1, Vector2I p2) => new Vector2I(p1.X % p2.X, p1.Y % p2.Y);
 
+        public static Vector2I operator +(Vector2I p1, int p2) => new Vector2I(p1.X + p2, p1.Y + p2);
+        public static Vector2I operator -(Vector2I p1, int p2) => new Vector2I(p1.X - p2, p1.Y - p2);
+        public static Vector2I operator *(Vector2I p1, int p2) => new Vector2I(p1.X * p2, p1.Y * p2);
+        public static Vector2I operator /(Vector2I p1, int p2) => new Vector2I(p1.X / p2, p1.Y / p2);
+        public static Vector2I operator %(Vector2I p1, int p2) => new Vector2I(p1.X % p2, p1.Y % p2);
+
         public static implicit operator Vector2I(Vector2 v) => new Vector2I(v);
         public static implicit operator Vector2(Vector2I v) => new Vector2(v.X, v.Y);
 
