817a636e2b24ff09f92ec8392f2d44076346c8b2
pandepic <39251913+pandepic@users.noreply.github.com>
2021-04-18 03:42:43 +1000

    basic transform helper stuff

---
+++ ECS/CommonComponents/TransformComponent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementEngine.ECS.CommonComponents
+{
+    public struct TransformComponent
+    {
+        public Entity Parent;
+        public float Rotation;
+        public Vector2 Position;
+        public Vector2 SectorPosition;
+
+        public Vector2 TransformedPosition
+        {
+            get
+            {
+                if (!Parent.IsAlive)
+                    return Position;
+                else
+                {
+                    ref var parentTransform = ref Parent.GetComponent<TransformComponent>();
+                    var transformMatrix =
+                        Matrix3x2.CreateRotation(parentTransform.Rotation) *
+                        Matrix3x2.CreateTranslation(parentTransform.TransformedPosition);
+
+                    return Vector2.Transform(Position, transformMatrix);
+                }
+            }
+        }
+
+        public Vector2 TransformedSectorPosition
+        {
+            get
+            {
+                if (!Parent.IsAlive)
+                    return SectorPosition;
+                else
+                {
+                    ref var parentTransform = ref Parent.GetComponent<TransformComponent>();
+                    return parentTransform.TransformedSectorPosition;
+                }
+            }
+        }
+    } // TransformComponent
+}
--- Math/Transform2D.cs
+++ Math/Transform2D.cs
@@ -5,10 +5,37 @@ using System.Text;
 
 namespace ElementEngine
 {
-    //public struct Transform2D
-    //{
-    //    public Vector2 Position;
-    //    public Vector2 Scale;
-    //    public float Rotation;
-    //}
+    public class Transform2D
+    {
+        public Transform2D Parent;
+        public float Rotation;
+
+        protected Vector2 _position;
+        public Vector2 Position
+        {
+            get
+            {
+                if (Parent == null)
+                    return _position;
+
+                var transformMatrix =
+                        Matrix3x2.CreateRotation(Parent.Rotation) *
+                        Matrix3x2.CreateTranslation(Parent.Position);
+
+                return Vector2.Transform(Position, transformMatrix);
+            }
+
+            set
+            {
+                _position = value;
+            }
+        }
+
+        public Transform2D(Vector2 position, float rotation = 0f, Transform2D parent = null)
+        {
+            _position = position;
+            Rotation = rotation;
+            Parent = parent;
+        }
+    } // Transform2D
 }
