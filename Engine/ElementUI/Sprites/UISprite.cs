f1e75bb14c94f9aaecb630e3637737a3a2173eb9
pandepic <39251913+pandepic@users.noreply.github.com>
2023-06-13 22:28:03 +1000

    reverting imgui dependency versions until the crash in them is fixed

--- Engine/ElementUI/Sprites/UISprite.cs
+++ Engine/ElementUI/Sprites/UISprite.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElementEngine.ElementUI
+﻿namespace ElementEngine.ElementUI
 {
     public class UISprite
     {
@@ -34,6 +27,5 @@ namespace ElementEngine.ElementUI
             position += Margin.TopLeft;
             return position;
         }
-
-    } // UISprite
+    }
 }
